fdd77f487ac0468492b797174d8adb7d7502eae6
sschreyer <samschreyer360@gmail.com>
2022-09-06 20:42:25 +1000

    Add form for adding novels to frontend

--- frontend/src/pages/NovelList.js
+++ frontend/src/pages/NovelList.js
@@ -1,8 +1,47 @@
+import React from 'react';
+
+import { 
+  TextField,
+  Button,
+  FormControl
+} from '@mui/material';
 
 function NovelList () {
+  const [novels, setNovels] = React.useState([]);
+
+  function updateNewNovel() {
+    
+  }
+
+  async function addNovel() {
+
+  }
+
   return (
     <>
-      List of novels here
+
+      { /* Display all novels */
+        novels.map((q, i) => {
+          return (
+          <div>
+            A novel will be displayed here
+          </div>);
+        })
+      }
+
+
+      {/* Form to submit new novels */}
+      <FormControl>
+        <TextField id="novel-title" label="Novel Title" variant="outlined" />
+        <TextField id="novel-descrip" label="Novel Description" variant="outlined" />
+        <Button
+          color='primary'
+          variant='outlined'
+          onClick={addNovel}
+        >
+          Add Novel
+        </Button>
+      </FormControl>
     </>
   );
 }
